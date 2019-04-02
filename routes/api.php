b30b4d359a6fbf4b6c84cb46e4a4cc8460d7ea89
jccosta <juliocostaa@icloud.com>
2019-04-01 23:11:24 -0300

    Initial Commit

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+use Illuminate\Http\Request;
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+    Route::get('signup/activate/{token}', 'AuthController@signupActivate');
+
+    Route::get('auth/redirect', 'AuthController@redirect');
+Route::get('auth/callback', 'AuthController@callback');
+  
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+});
